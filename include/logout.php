897fad88d264a4525423f55263ebb1597fa12983
WrightProjects <wright@ogarnij.se>
2018-12-04 20:25:24 +0100

    :<

--- include/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	if (empty($_SESSION['user_online'])) {
-		session_unset();
-		$_SESSION['alert'] = array('message' => 'Wylogowano pomyślnie!', 'type' => 'success');
-		exit(header('Location: ?login'));
-	}
-
-	header('Location: ?login');
